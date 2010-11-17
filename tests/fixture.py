779456d81b2c9175cbbe311399cf843e6058bead
Quinn Slack <sqs@cs.stanford.edu>
2010-11-17 10:50:48 -0800

    set port on localhost fixtures

--- tests/fixture.py
+++ tests/fixture.py
@@ -1,4 +1,4 @@
 from qjam.node import Node
-localhost_nodes = (Node('localhost', root='/tmp/qjam1/'),
-                   Node('localhost', root='/tmp/qjam2/'))
+localhost_nodes = (Node('localhost', port=2000, root='/tmp/qjam1/'),
+                   Node('localhost', port=2001, root='/tmp/qjam2/'))
 
