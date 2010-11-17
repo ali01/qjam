bff76261376099721d066fbf12ea7dddc258cb18
Quinn Slack <sqs@cs.stanford.edu>
2010-11-16 17:02:58 -0800

    NumPyMatrixDataSet

--- qjam/dataset.py
+++ qjam/dataset.py
@@ -1,14 +1,19 @@
 """DataSet objects are wrappers for some underlying data that expose an
 interface for slicing up the underlying data into slices for single nodes."""
 
+import numpy
+
 def DataSet(o):
     if isinstance(o, BaseDataSet):
         return o
     elif isinstance(o, list) or isinstance(o, tuple):
         return ListDataSet(o)
+    elif isinstance(o, numpy.ndarray):
+        return NumPyMatrixDataSet(o)
 
 class BaseDataSet(object):
-    pass
+    def slices(self, n_nodes):
+        return [self.slice(n_nodes, i) for i in range(n_nodes)]
     
 class ListDataSet(BaseDataSet):
     def __init__(self, _list):
@@ -18,5 +23,14 @@ class ListDataSet(BaseDataSet):
         slice_size = (len(self.list)+n_nodes) / n_nodes # round up in division
         return self.list[slice_size*i:slice_size*(i+1)]
 
-    def slices(self, n_nodes):
-        return [self.slice(n_nodes, i) for i in range(n_nodes)]
+class NumPyMatrixDataSet(BaseDataSet):
+    """Matrix whose rows are training examples."""
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def slice(self, n_nodes, i):
+        nrows = numpy.size(self.matrix, 0)
+        slice_rows = (nrows + n_nodes) / n_nodes # round up in division
+        return self.matrix[slice_rows*i:slice_rows*(i+1)]
+
+    
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy
 from . import fixture
 from qjam import DataSet
 
@@ -12,3 +13,13 @@ class TestListDataSet(unittest.TestCase):
         self.assertEqual([1,2], slices[0])
         self.assertEqual([3,4], slices[1])
         self.assertEqual([5], slices[2])
+
+class TestNumPyMatrixDataSet(unittest.TestCase):
+    def test_slices(self):
+        x = numpy.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
+        ds = DataSet(x)
+        slices = ds.slices(3)
+        self.assertTrue((numpy.array([[1,2],[3,4]]) == slices[0]).all())
+        self.assertTrue((numpy.array([[5,6],[7,8]]) == slices[1]).all())
+        self.assertTrue((numpy.array([[9,10]]) == slices[2]).all())
+        
