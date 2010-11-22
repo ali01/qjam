8f09bc859f4494e0fb0430f17b47421816f5b7e0
Matt Sparks <ms@quadpoint.org>
2010-11-21 23:32:44 -0600

    Successful serialization and execution of simple task

--- qjam/worker/worker.py
+++ qjam/worker/worker.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python2.6
+import base64
+import cPickle as pickle
 import json
 import logging
+import marshal
 import os
 import sys
+import types
 
 
 def read_message():
@@ -12,20 +16,48 @@ def read_message():
 
 
 def print_message(msg):
-  print msg
+  logging.debug('received message: %s' % msg)
 
 
 def handle_message(msg):
-  if msg['cmd'] == 'task':
+  msg_type = msg['type']
+  if msg_type == 'task':
     handle_task_message(msg)
-  elif msg['cmd'] == 'refs':
+  elif msg_type == 'refs':
     handle_refs_message(msg)
   else:
-    logging.warning('unexpected command: %s' % msg['cmd'])
+    logging.warning('unexpected message type: %s' % msg_type)
+
+
+def handle_task_message(msg):
+  # Sanity check.
+  for key in ('callable', 'params', 'dataset'):
+    if key not in msg:
+      logging.warning('incomplete message: missing key "%s"' % key)
+      return
+
+  code = marshal.loads(base64.b64decode(msg['callable']))
+  callable = types.FunctionType(code, globals(), 'foo')
+  params = msg['params']
+  dataset = msg['dataset']
+
+  # For now, assume no dataset.
+  send_message('state', {'status': 'running'})
+
+  # Run the callable.
+  result = callable(params, dataset)
+
+  # Send the result to the master.
+  enc_result = base64.b64encode(pickle.dumps(result))
+  send_message('result', {'result': enc_result})
+
+
+def handle_refs_message(msg):
+  pass
 
 
 def process_message(msg):
-  if 'cmd' not in msg:
+  if 'type' not in msg:
     # Ill-formed message.
     raise ValueError
 
@@ -33,18 +65,27 @@ def process_message(msg):
   handle_message(msg)
 
 
+def send_message(msg_type, msg):
+  msg['type'] = msg_type
+  sys.stdout.write('%s\n' % json.dumps(msg))
+  sys.stdout.flush()
+
+
 def main():
   # Set up logging.
   _fmt = '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-  logging.basicConfig(level=logging.DEBUG, format=_fmt)
+  logging.basicConfig(level=logging.WARNING, format=_fmt)
 
   while True:
     msg_str = read_message()
+    if not msg_str:
+      return
+
     try:
       msg = json.loads(msg_str)
       process_message(msg)
-    except ValueError:
-      logging.warning('received ill-formed message: %s' % msg_str)
+    except ValueError, e:
+      logging.warning('received ill-formed message: %s (%s)' % (msg_str, e))
 
 
 if __name__ == '__main__':
--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,10 +1,17 @@
 from nose.tools import *
+import base64
+import cPickle as pickle
 import json
+import marshal
 import os
 import subprocess
 import sys
 
 import qjam.worker.worker
+
+
+def constant(params, dataset):
+  return 42
 
 
 class Test_Worker:
@@ -14,17 +21,31 @@ class Test_Worker:
     self._worker = subprocess.Popen(_path,
                                     stdin=subprocess.PIPE,
                                     stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
+                                    stderr=subprocess.PIPE,
+                                    bufsize=0)
 
   def teardown(self):
     self._worker.kill()
+
+  def read_stderr(self):
+    return self._worker.stderr.readline()
+
+  def read_message(self):
+    msg_str = self._worker.stdout.readline()
+    return json.loads(msg_str.strip())
 
   def write_message(self, msg):
     self._worker.stdin.write('%s\n' % json.dumps(msg))
 
   def write_command(self, cmd, data):
-    data['cmd'] = cmd
+    data['type'] = cmd
     self.write_message(data)
+
+  def encode_callable(self, callable):
+    return base64.b64encode(marshal.dumps(callable.func_code))
+
+  def decode_result(self, data):
+    return pickle.loads(base64.b64decode(data))
 
   def test_process(self):
     assert_not_equals(self._worker.stdin, None)
@@ -33,10 +54,38 @@ class Test_Worker:
 
   def test_bad_message(self):
     self.write_message({'bogus_key': 1234})
-    line = self._worker.stderr.readline()
-    assert_true('ill-formed' in line)
+    assert_true('ill-formed' in self.read_stderr())
 
-  def test_bad_command(self):
+  def test_bad_type(self):
     self.write_command('bogus_command', {})
-    line = self._worker.stderr.readline()
-    assert_true('unexpected command' in line)
+    assert_true('unexpected message type' in self.read_stderr())
+
+  def test_incomplete_task(self):
+    c = self.encode_callable(constant)
+    msg1 = {'callable': c,
+            'params': ()}
+    msg2 = {'callable': c,
+            'dataset': None}
+    msg3 = {'dataset': None,
+            'params': ()}
+
+    for msg in (msg1, msg2, msg3):
+      self.write_command('task', msg)
+      assert_true('missing key' in self.read_stderr())
+
+  def test_simple_task(self):
+    msg = {'callable': self.encode_callable(constant),
+           'params': None,
+           'dataset': None}
+    self.write_command('task', msg)
+
+    state_msg = self.read_message()
+    assert_true('type' in state_msg)
+    assert_equal('state', state_msg['type'])
+    assert_true('status' in state_msg)
+    assert_equal('running', state_msg['status'])
+
+    result_msg = self.read_message()
+    assert_true('result' in result_msg)
+    result = self.decode_result(result_msg['result'])
+    assert_equals(42, result)
