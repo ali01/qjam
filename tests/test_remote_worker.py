df6869ed014bf33213228e5acaed21b191bde369
Ali Yahya <alive@ali01.com>
2010-11-23 00:30:03 -0600

    added basic tests for remote_worker

---
+++ tests/test_remote_worker.py
@@ -0,0 +1,24 @@
+from nose.tools import *
+
+from qjam.master.remote_worker import RemoteWorker
+from qjam.msg.task_msg import TaskMsg
+
+# test modules
+import constant
+import sum_params
+
+class TestRemoteWorker:
+  def setup(self):
+    self.remote_worker = RemoteWorker('localhost')
+
+  def test_task_constant(self):
+    task_msg = TaskMsg(constant, None, [])
+    self.remote_worker.taskIs(task_msg)
+    assert_equals(42, self.remote_worker.result())
+
+
+  def test_task_sum(self):
+    params = [1, 2, 3, 6, 7, 9]
+    task_msg = TaskMsg(sum_params, None, params)
+    self.remote_worker.taskIs(task_msg)
+    assert_equals(sum(params), self.remote_worker.result())
