5083790253bb7b617093a87962d80ca74a9c6f9c
Ali Yahya <alive@ali01.com>
2010-11-24 10:33:40 -0600

    completed implementation of task_msg with new dataset

--- qjam/msg/task_msg.py
+++ qjam/msg/task_msg.py
@@ -2,6 +2,7 @@ import base64
 import cPickle as pickle
 import json
 
+from qjam.dataset import DataSet, BaseDataSet
 from qjam.utils import module_src
 from base_msg import BaseMsg, encode_msg_field, decode_msg_field
 
@@ -10,20 +11,21 @@ class TaskMsg(BaseMsg):
     BaseMsg.__init__(self, 'task')
     self.__module  = module_object
     self.__params  = params
-    if dataset != None:
-      if not isinstance(dataset, list):
-        exc_msg = 'expected list of refs or None for dataset'
-        # TODO(ali01): logging
-        raise ValueError, exc_msg
+    if dataset != None and not issubclass(type(dataset), BaseDataSet):
+      exc_msg = 'expected object of type DataSet or None for dataset param.'
+      # TODO(ali01): logging
+      raise ValueError, exc_msg
 
-      self.__dataset = dataset
-
-    else:
-      self.__dataset = []
+    self.__dataset = dataset
 
   def json_str(self):
+    if self.__dataset == None:
+      ref_list = []
+    else:
+      ref_list = self.__dataset.hash_list()
+
     msg = {'type': self.type(),
            'module':  encode_msg_field(module_src(self.__module)),
-           'dataset': self.__dataset,
+           'dataset': ref_list,
            'params':  encode_msg_field(self.__params)}
     return json.dumps(msg)
