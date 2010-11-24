c7f25fcbcadb87ab2211b94ccbe663c00709357d
Ali Yahya <alive@ali01.com>
2010-11-24 05:15:52 -0600

    prelim implementation of Master

--- qjam/master/master.py
+++ qjam/master/master.py
@@ -6,16 +6,43 @@ import json
 import cPickle as pickle
 import paramiko
 
+from remote_worker import RemoteWorker
+from remote_task_thread import RemoteTaskThread
 
-class Master(worker_hostnames):
-  def __init__(self, workers):
-    self.__workers = workers
+from qjam.msg.task_msg import TaskMsg
+from qjam.common.reducing import default_reduce
 
-  def run(self, job):
-    '''Distributes data to workers and then runs JOB on all workers.
-       Returns the sum of the workers responses'''
-    
-    # distribute data and run tasks
-    for i,worker in enumerate(self.__workers):
-      # distribute data
-      
+class Master(object):
+  def __init__(self, remote_workers):
+    if not isinstance(remote_workers, list):
+      raise TypeError, 'remote_workers parameter must be of type list'
+
+    if len(remote_workers) == 0:
+      raise ValueError, 'list of workers cannot be empty'
+
+    for worker in remote_workers:
+      if not isinstance(worker, RemoteWorker):
+        exc_msg = '''elements in the parameter list, remote_workers,
+                     must be of type RemoteWorker'''
+        raise TypeErrer, exc_msg
+
+    self.__workers = remote_workers
+    self.__thread_pool = []
+
+  def run(self, module, params=None, dataset=None):
+    for worker in self.__workers:
+      # TODO: pass in appropriate slice
+      task_msg = TaskMsg(module, params, None)
+      thread = RemoteTaskThread(worker, task_msg)
+      self.__thread_pool.append(thread)
+      thread.start()
+
+    for thread in self.__thread_pool:
+      thread.join()
+
+    results = []
+    for thread in self.__thread_pool:
+      results.append(thread.result())
+
+    # TODO: look for custom reduce function
+    return reduce(default_reduce, results)
