159c8d65b3592734165b54672a3649830306cfb9
Ali Yahya <alive@ali01.com>
2010-11-24 02:59:41 -0600

    remote_worker now copies all required common files over to remote node

--- qjam/__init__.py
+++
@@ -1,4 +0,0 @@
-from .job import Job
-from .dataset import DataSet
-from .master.master import Master
-from .node.node import Node
--- tests/test_remote_worker.py
+++ tests/test_remote_worker.py
@@ -1,5 +1,6 @@
 from nose.tools import *
 
+from qjam.master.remote_task_thread import RemoteTaskThread
 from qjam.master.remote_worker import RemoteWorker
 from qjam.msg.task_msg import TaskMsg
 
@@ -22,3 +23,24 @@ class TestRemoteWorker:
     task_msg = TaskMsg(sum_params, params, dataset=None)
     self.remote_worker.taskIs(task_msg)
     assert_equals(sum(params), self.remote_worker.result())
+
+
+class TestRemoteTaskThread:
+  def setup(self):
+    self.remote_worker = RemoteWorker('localhost')
+
+  def test_task_constant(self):
+    task_msg = TaskMsg(constant, params=None, dataset=None)
+    task_thread = RemoteTaskThread(self.remote_worker, task_msg);
+    task_thread.start()
+    task_thread.join()
+    assert_equals(42, task_thread.result())
+
+
+  def test_task_sum(self):
+    params = [1, 2, 3, 6, 7, 9]
+    task_msg = TaskMsg(sum_params, params, dataset=None)
+    task_thread = RemoteTaskThread(self.remote_worker, task_msg);
+    task_thread.start()
+    task_thread.join()
+    assert_equals(sum(params), task_thread.result())
