5a4e793c72c1f568e45cf0df787837836fde0a67
Quinn Slack <sqs@cs.stanford.edu>
2010-11-24 14:05:43 -0800

    update qjam-run.py to use new interface

--- bin/qjam-run.py
+++ bin/qjam-run.py
@@ -7,15 +7,16 @@ import sys
 # Add parent directory to path.
 sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 
-from qjam import Node, Master, Job
+from qjam import DataSet
+from qjam.master import Master, RemoteWorker
 
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 logger = logging.getLogger('qjam')
 
 def parse_args():
     parser = OptionParser(usage="%prog [opts] <mapfunc> <dataset> [params]")
-    parser.add_option("-n", "--nodes", dest="nodes", default="localhost",
-                      help="list of nodes, e.g. 'node1 node2'")
+    parser.add_option("-w", "--workers", dest="workers", default="localhost",
+                      help="list of workers, e.g. 'host1 host2'")
     parser.add_option("-q", "--quiet", dest="verbose",
                       action="store_false",
                       help="quiet (default is verbose)")
@@ -24,17 +25,20 @@ def parse_args():
         parser.error("missing required args <mapfunc> <dataset>")
     return (options, args)
 
-def print_nodes(nodes):
-    logger.debug("qjam using %d nodes:" % len(nodes))
-    for node in nodes:
-        logger.debug("  %s"  % node)
+def print_workers(workers):
+    logger.debug("qjam using %d workers:" % len(workers))
+    for worker in workers:
+        logger.debug("  %s"  % worker)
 
+def resolve_module(name):
+    __import__(name)
+    return sys.modules[name]
+        
 def resolve_module_attr(name):
     """Returns the `someattr` attribute in `module1.module2` given the string
     'module1.module2.someattr` """
     hier, attr_name = name.rsplit('.', 1)
-    __import__(hier)
-    mod = sys.modules[hier]
+    mod = resolve_module(hier)
     attr_val = getattr(mod, attr_name)
     return attr_val
         
@@ -46,20 +50,18 @@ def main():
     params_name = args[2] if len(args) == 3 else None
 
     # set up cluster
-    nodes = [Node(host) for host in options.nodes.split()]
-    print_nodes(nodes)
-    master = Master(nodes)
+    workers = [RemoteWorker(host) for host in options.workers.split()]
+    print_workers(workers)
+    master = Master(workers)
 
     # set up job
-    # TODO: allow user to specify params - through a separate .py file?
-    mapfunc = resolve_module_attr(mapfunc_name)
+    mapfunc_mod = resolve_module(mapfunc_name)
     dataset = resolve_module_attr(dataset_name)
     params = resolve_module_attr(params_name) if params_name else None
-    job_name = mapfunc_name.rsplit('.', 1)[1]
-    job = Job(mapfunc, name=job_name, dataset=dataset, params=params)
 
     # run
-    result = master.run(job)
+    dataset = DataSet(list(dataset))
+    result = master.run(mapfunc_mod, dataset=dataset, params=params)
     print result
 
 if __name__ == "__main__":
