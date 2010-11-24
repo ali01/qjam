9131b73ffc35470f829dbc528b540863fcd03fc3
Ali Yahya <alive@ali01.com>
2010-11-24 03:15:55 -0600

    Revert "deleted job and merged prev master with new master"
    This reverts commit ace2f449dd06f7cdf925fd34adeb093ee80e31cf.

---
+++ qjam/master/master_new.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import os
+import threading
+import base64
+import json
+import cPickle as pickle
+import paramiko
+
+
+class Master(workers):
+  def __init__(self, workers):
+    self.__workers = workers
+
+  def run(self, job):
+    '''Distributes data to workers and then runs JOB on all workers.
+    Returns the sum of the workers responses'''
+    
+    # distribute data and run tasks
+    for i,worker in enumerate(self.__workers):
+      # distribute data
+      
