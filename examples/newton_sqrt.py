f25a29effd364d57350003cf7ba0f99889921f0a
Quinn Slack <sqs@cs.stanford.edu>
2010-11-25 13:04:18 -0600

    add newton_sqrt example

---
+++ examples/newton_sqrt.py
@@ -0,0 +1,31 @@
+def mapfunc(n, x):
+    """Performs Newton's method to approximate sqrt(n)."""
+    x = x[0]
+    return x - (x*x - n)/(2*x)
+
+if __name__ == "__main__":
+    import sys
+    from qjam import DataSet
+    from qjam.master import Master, RemoteWorker
+
+    if len(sys.argv) != 2:
+        print "approximates sqrt(n)\nusage: %s <n>" % sys.argv[0]
+        exit(1)
+    n = float(sys.argv[1])
+
+    master = Master([RemoteWorker('localhost')])
+    x0 = DataSet([float(10)])
+    mod = sys.modules[__name__]
+
+    eps = 0.0001
+    xs = [float(10.0)]
+    i = 1
+    while True:
+        x = master.run(mod, params=n, dataset=DataSet([xs[-1]]))
+        dx = abs(x - xs[-1])
+        xs.append(x)
+        print "%d. %f (delta=%f)" % (i, x, dx)
+        if dx < eps:
+            break
+        i += 1
+
